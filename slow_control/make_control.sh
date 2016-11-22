c3d13e8138e2ae61009bb7e457ddcb3a343e69a5
SATO Yutaro <syutaro@cx03.cc.kek.jp>
2016-11-22 10:14:48 +0900

    second commit

---
+++ slow_control/make_control.sh
@@ -0,0 +1,44 @@
+#! /bin/tcsh -f
+
+if( $#argv < 3 )then
+    echo " Usage : $0  [chip_no] [channel]    [command]    (command for others)"
+    echo "Example: $0   1111111      3    LLLLLLLLLLLLLHHH  (LLLLLLLLLLLLLLLL)"
+    echo "           chip_no : 7 bit"
+    echo "           channel : 0-127"
+    echo "           command(16 bit) for specific channel"
+    echo "           command(16 bit) for other channels : default(LLLLLLLLLLLLLLLL)"
+    exit 1
+endif
+
+set OUTFILE       = "control.dat"
+set CHIP_NO       = $1
+set CHANNEL_NO    = $2
+set COMMAND       = $3
+if( $#argv > 3 )then
+   set COMMAND_OTHER = $4
+else
+   set COMMAND_OTHER = "LLLLLLLLLLLLLLLL"
+endif
+
+  
+echo "#"            >  ${OUTFILE}
+echo "# dip switch" >> ${OUTFILE}
+echo ${CHIP_NO}     >> ${OUTFILE}
+echo "#"            >> ${OUTFILE}
+
+set CNT_CH = 0
+while( 1 )
+  printf "%-5s" ${CNT_CH} >> ${OUTFILE}
+  if( ${CNT_CH} == ${CHANNEL_NO} ) then
+    echo ${COMMAND} >> ${OUTFILE}
+  else
+    echo ${COMMAND_OTHER} >> ${OUTFILE}
+  endif
+
+    
+  if( ${CNT_CH} == 127) then
+    break;
+  endif
+  @ CNT_CH += 1
+end
+  
